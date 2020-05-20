a2b102705bc2ebf0c98e37acc87f3f085ad3a3ac
Warlock <b.jurczok@gmail.com>
2020-05-20 21:35:22 +0200

    simple markdown compiler

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,7 +1,79 @@
-fn parse_markdown_file( _filename: &str) {
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+use std::path::Path;
+
+fn parse_markdown_file(_filename: &str) {
     print_short_banner();
     println!("[ INFO ] Trying to parse {} ...", _filename);
 
+    let input_file = Path::new(_filename);
+
+    let file = File::open(&input_file).expect("[ ERROR ] Failed to open a file");
+
+    let mut _ptag = false;
+    let mut _htag = false;
+
+    let mut tokens: Vec<String> = Vec::new();
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line_contents = line.unwrap();
+        let mut first_char: Vec<char> = line_contents.chars().take(1).collect();
+        let mut output_line = String::new();
+
+        match first_char.pop() {
+            Some('#') => {
+                if _ptag {
+                    _ptag = false;
+                    output_line.push_str("</p>\n");
+                }
+
+                if _htag {
+                    _htag = false;
+                    output_line.push_str("</h1>\n");
+                }
+
+                _htag = true;
+                output_line.push_str("\n\n<h1>");
+                output_line.push_str(&line_contents[2..]);
+            }
+            _ => {
+                if !_ptag {
+                    _ptag = true;
+                    output_line.push_str("<p>");
+                }
+                output_line.push_str(&line_contents);
+            }
+        }
+
+        if _ptag {
+            _ptag = false;
+            output_line.push_str("</p>\n");
+        }
+
+        if _htag {
+            _htag = false;
+            output_line.push_str("</h1>\n");
+        }
+
+        if output_line != "<p></p>\n" {
+            tokens.push(output_line);
+        }
+    }
+
+    let mut output = String::from(&_filename[.._filename.len() - 2]);
+    output.push_str("html");
+
+    let mut outfile = File::create(output).expect("[ ERROR ] Could not create output file!");
+
+    for line in &tokens {
+        outfile
+            .write_all(line.as_bytes())
+            .expect("[ ERROR ] Could not write to output file!");
+    }
+
+    println!("[ INFO ] Parsing complete!");
 }
 
 fn print_long_banner() {
@@ -21,7 +93,7 @@ fn get_title() -> String {
     thr_title.push_str(env!("CARGO_PKG_VERSION"));
     thr_title.push_str("), ");
     thr_title.push_str(env!("CARGO_PKG_DESCRIPTION"));
-    thr_title.push_str("A tiny markdown compiler based on Jesse's tutorials.");
+    thr_title.push_str("A tiny markdown compiler.");
     thr_title
 }
 
@@ -31,7 +103,7 @@ fn usage() {
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    
+
     match args.len() {
         2 => parse_markdown_file(&args[1]),
         _ => {
